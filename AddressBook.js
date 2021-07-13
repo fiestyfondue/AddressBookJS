7c79b0a33461343fcfc046364b6576f485c5989d
Harsh <harsh.21483@gmail.com>
2021-07-13 08:11:25 -0700

    [Harsh]Add:-Constructor is added and all the required fields are alloted

---
+++ AddressBook.js
@@ -0,0 +1,12 @@
+console.log('Welcome to Address Book');
+
+function Contact(FirstName,LastName,Address,City,State,Zip,PhoneNumber,Email){
+    this.FirstName=firstName;
+    this.LastName=lastName;
+    this.Address=address;
+    this.City=city;
+    this.State=state;
+    this.Zip=zip;
+    this.PhoneNumber=phonenumber;
+    this.Email=email;
+}
