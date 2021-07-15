b931a8d8014bbf3aa935dd565a440067c4160950
fiestyfondue <84001951+fiestyfondue@users.noreply.github.com>
2021-07-15 10:26:47 -0700

    Update AddressBook.js

--- AddressBook.js
+++ AddressBook.js
@@ -1,12 +1,55 @@
-console.log('Welcome to Address Book');
+  
+class AddressBook{
+    constructor(...para){
+        this.firstName = para[0];
+        this.lastName = para[1];
+        this.address = para[2];
+        this.city = para[3];
+        this.state = para[4];
+        this.zip = para[5];
+        this.phoneNum = para[6];
+        this.email = para[7];
+    }
 
-function Contact(FirstName,LastName,Address,City,State,Zip,PhoneNumber,Email){
-    this.FirstName=firstName;
-    this.LastName=lastName;
-    this.Address=address;
-    this.City=city;
-    this.State=state;
-    this.Zip=zip;
-    this.PhoneNumber=phonenumber;
-    this.Email=email;
+    get firstName(){return this._firstName}
+    set firstName(firstName){
+        this._firstName = firstName;
+    }
+
+    get lastName(){return this._lastName}
+    set lastName(lastName){
+        this._lastName = lastName;
+    }
+    
+    get address(){return this._address}
+    set address(address){
+        this._address = address;
+    }
+    
+    get city(){return this._city}
+    set city(city){
+        this._city = city;
+    }
+
+    get zip(){return this._zip}
+    set zip(zip){
+        this._zip = zip;
+    }
+
+    get phoneNum(){return this._phoneNum}
+    set phoneNum(phoneNum){
+        this._phoneNum = phoneNum;
+    }
+    
+    get email(){return this._email}
+    set email(email){
+        this._email = email;
+    }
+
+    toString(){
+        return "\nFirstName = "+ this.firstName +", \nlastName = "+ this.lastName+ ", \nAddress = "+ this.address +", \nCity = "+ this.city +", \nState = "+ this.state +", \nZip = "+ this.zip +", \nPhoneNo = "+ this.phoneNum +", \nEmail = "+ this.email;
+    }
 }
+
+let addressBook = new AddressBook("Harsh","Sinha","Bhopal","HoshangabadRoad","MP","462024","567567698","harsh.21@gmail.com")
+console.log(addressBook.toString());
