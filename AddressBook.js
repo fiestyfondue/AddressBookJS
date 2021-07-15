05eeb59a5c53a9da9baea1a6b9a80d8f67d56d23
Harsh <harsh.21483@gmail.com>
2021-07-15 10:46:07 -0700

    [Harsh]Refactor:- Regex validation is performed on the attributes

--- AddressBook.js
+++ AddressBook.js
@@ -1,12 +1,81 @@
 console.log('Welcome to Address Book');
-
-function Contact(FirstName,LastName,Address,City,State,Zip,PhoneNumber,Email){
-    this.FirstName=firstName;
-    this.LastName=lastName;
-    this.Address=address;
-    this.City=city;
-    this.State=state;
-    this.Zip=zip;
-    this.PhoneNumber=phonenumber;
-    this.Email=email;
+const input = require('prompt-sync')();
+class Contact{
+    ContactBook(...parameters){
+        this.firstName = parameters[0];
+        this.lastName = parameters[1];
+        this.address = parameters[2];
+        this.city = parameters[3];
+        this.state = parameters[4];
+        this.zip = parameters[5];
+        this.phoneNo = parameters[6];
+        this.email = parameters[7];
+    }
+    get firstName(){return this._firstName}
+    set firstName(firstName){
+        let nameRegex=RegExp('^[A-Z]{1}[a-zA-Z]{2,}$')
+        if(nameRegex.test(firstName)){
+            this._firstName=firstName
+        }
+        else throw "Invalid value entered"
+    }
+    get lastName(){return this._lastName}
+    set lastName(lastName){
+        let nameRegex=RegExp('^[A-Z]{1}[a-zA-Z]{2,}$')
+        if(nameRegex.test(lastName)){
+            this._lastName=lastName
+        }
+        else throw "Invalid Value entered"
+    }
+    get address(){ return this._address}
+    set address(address){
+        let regex = RegExp('[a-zA-Z0-9 ,]{4,}')
+        if(regex.test(address)) {
+            this._address = address
+        }
+        else throw "Incorrect Address"
+    }
+    get city(){ return this._city}
+    set city(city){
+        let regex = RegExp('[a-zA-Z0-9 ,]{4,}')
+        if(regex.test(city)) {
+            this._city = city
+        }
+        else throw "Incorrect City"
+    }
+    get state(){ return this._state}
+    set state(state){
+        let regex = RegExp('[a-zA-Z0-9 ,]{4,}')
+        if(regex.test(state))
+            this._state = state
+        else throw "Incorrect State"
+    }
+    get zip(){ return this._zip}
+    set zip(zip){
+        let pinRegex = RegExp("^[0-9]{3}\s{0,1}[0-9]{3}$")
+        if(pinRegex.test(zip)){
+            this._zip = zip
+        }
+        else throw "Incorrect Zip"
+    }
+    get phoneNo(){ return this._phoneNo}
+    set phoneNo(phoneNo){
+        let regex = RegExp('[0-9]{2}\\s[0-9]{10}')
+        if(regex.test(phoneNo)) {
+            this._phoneNo = phoneNo
+        }
+        else throw "Incorrect Phone Number"
+    }
+    get email(){ return this._email}
+    set email(email){
+        let regex = RegExp('^[a-z]+([_+-.]?[a-z0-9]+)*[@][a-z0-9]+[.]([a-z]+){2,}([.]?[a-z]{2})?$')
+        if(regex.test(email)) {
+            this._email = email
+        }
+        else throw "Incorrect Email"
+    }
+    toString(){
+        return "\n\nFirstName: " + this.firstName + "\nLastName: " + this.lastName + "\nAddress: " + this.address + "\nCity: " + this.city +
+                "\nState: " + this.state + "\nZip: " + this.zip + "\nPhoneNO: " + this.phoneNo + "\nEmail: " + this.email;
+    }
 }
